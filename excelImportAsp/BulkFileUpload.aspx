e45ec416684383e1236af2ff160186e7c2cf10f1
mahikero <federicoparassarmiento@yahoo.com>
2016-01-29 16:03:15 -0500

    Ini

---
+++ excelImportAsp/BulkFileUpload.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="BulkFileUpload.aspx.cs" Inherits="excelImportAsp.BulkFileUpload" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h3>Code Scratcher</h3>
+        <p>
+
+            <asp:Button ID="btnUpload" runat="server" Text="SQL Bulk Upload" OnClick="btnUpload_Click" />
+
+        </p>
+
+
+    </div>
+        <asp:Label ID="lbl_Message" runat="server" Visible="false"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ excelImportAsp/BulkFileUpload.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+using System.Configuration;
+
+namespace excelImportAsp
+{
+    public partial class BulkFileUpload : System.Web.UI.Page
+    {
+        String excelConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            ExcelBulkUpload();
+        }
+
+        private void ExcelBulkUpload() 
+        {
+            String strFileNmae = "StudentRecords.xlsx";
+            String strSheetName = "StudentDetails";
+            String path = Server.MapPath("Import") + "\\" + strFileNmae;
+            excelConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";Extended Properties=Excel 12.0;HDR=Yes;IMEX=2";
+        
+            OleDbConnection excelConnection = new OleDbConnection(excelConnectionString);
+            OleDbCommand cmd = new OleDbCommand("Select * from [" + strSheetName + "$]", excelConnection);
+            excelConnection.Open();
+
+            OleDbDataReader dReader;
+            dReader = cmd.ExecuteReader();
+
+            string strServer = ConfigurationManager.ConnectionStrings["StudentConnectionString"].ToString();
+
+            SqlBulkCopy sqlBulk = new SqlBulkCopy(strServer);
+            
+
+
+        }
+    }
+}
