e9415eda563a61f7ae5af1f743df42c983450ab0
mahikero <federicoparassarmiento@yahoo.com>
2016-02-01 08:04:41 -0500

    up

--- excelImportAsp/BulkFileUpload.aspx.cs
+++ excelImportAsp/BulkFileUpload.aspx.cs
@@ -28,35 +28,42 @@ namespace excelImportAsp
         {
             if (FileUpload1.HasFile)
             {
-                string path = string.Concat(Server.MapPath("~/Import/" + FileUpload1.FileName));
+                try
+                {
+                    string path = string.Concat(Server.MapPath("~/Import/" + FileUpload1.FileName));
 
-                FileUpload1.SaveAs(path);
+                    FileUpload1.SaveAs(path);
 
-                //string strFileNmae = "StudentRecords.xlsx";
-                string strSheetName = "StudentDetails";
-                //String path = Server.MapPath("Import") + "\\" + strFileNmae;
-                excelConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";Extended Properties='Excel 12.0;HDR=Yes;IMEX=2'";
+                    //string strFileNmae = "StudentRecords.xlsx";
+                    string strSheetName = "StudentDetails";
+                    //String path = Server.MapPath("Import") + "\\" + strFileNmae;
+                    excelConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";Extended Properties='Excel 12.0;HDR=Yes;IMEX=2'";
 
-                OleDbConnection excelConnection = new OleDbConnection(excelConnectionString);
-                OleDbCommand cmd = new OleDbCommand("Select * from [" + strSheetName + "$]", excelConnection);
-                excelConnection.Open();
+                    OleDbConnection excelConnection = new OleDbConnection(excelConnectionString);
+                    OleDbCommand cmd = new OleDbCommand("Select * from [" + strSheetName + "$]", excelConnection);
+                    excelConnection.Open();
 
-                OleDbDataReader dReader;
-                dReader = cmd.ExecuteReader();
+                    OleDbDataReader dReader;
+                    dReader = cmd.ExecuteReader();
 
-                string strServer = ConfigurationManager.ConnectionStrings["StudentConnectionString"].ToString();
+                    string strServer = ConfigurationManager.ConnectionStrings["StudentConnectionString"].ToString();
 
-                SqlBulkCopy sqlBulk = new SqlBulkCopy(strServer);
-                sqlBulk.DestinationTableName = "StudentDetails";
-                sqlBulk.ColumnMappings.Add("Roll No", "RollNo");
-                sqlBulk.ColumnMappings.Add("Student Name", "Name");
-                sqlBulk.ColumnMappings.Add("Department", "Department");
-                sqlBulk.ColumnMappings.Add("Section", "Section");
-                sqlBulk.WriteToServer(dReader);
+                    SqlBulkCopy sqlBulk = new SqlBulkCopy(strServer);
+                    sqlBulk.DestinationTableName = "StudentDetails";
+                    sqlBulk.ColumnMappings.Add("Roll No", "RollNo");
+                    sqlBulk.ColumnMappings.Add("Student Name", "Name");
+                    sqlBulk.ColumnMappings.Add("Department", "Department");
+                    sqlBulk.ColumnMappings.Add("Section", "Section");
+                    sqlBulk.WriteToServer(dReader);
 
-                excelConnection.Close();
-                lbl_Message.Text = "Your file data has been successfully uploaded";
-                lbl_Message.Visible = true;
+                    excelConnection.Close();
+                    lbl_Message.Text = "Your file data has been successfully uploaded";
+                    lbl_Message.Visible = true;
+                }
+                catch (Exception ex)
+                {
+                    lbl_Message.Text = ex.Message;
+                }
             }
         }
     }
